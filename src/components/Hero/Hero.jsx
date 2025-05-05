045df26e464e7da97c9e8f3300cfdbd0ce07fa7f
Collins Makui <52255997+Makcollins@users.noreply.github.com>
2025-05-05 12:41:39 +0300

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <p className={styles.description}>I enjoy creating beautiful web applications.
               If you're interested in learning more, feel free to reach out--I'd love to connect!
             </p>
-            <a href="mailto:makcollins16@gmail.com" className={styles.contactBtn}>Contact Me</a>
+            <a href="#contact" className={styles.contactBtn}>Contact Me</a>
         </div>
         <img src={heroImage()} alt="My Image" className={styles.heroImg}/>
         <div className={styles.topBlur}></div>
