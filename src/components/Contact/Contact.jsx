49e42730f3ac3df86ce2ccb4ad6c5f8dd306fed2
Makcollins <makcollins16@gmail.com>
2026-06-07 08:57:24 +0300

    modified github links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaCopyright, FaGithub, FaLinkedin, FaMailBulk, FaMailchimp, FaRegCopyright } from 'react-icons/fa';
+import { FaGithub, FaLinkedin,FaRegCopyright } from 'react-icons/fa';
 import { SiGmail } from 'react-icons/si';
 import styles from './Contact.module.css';
 
@@ -12,8 +12,9 @@ const Contact = () => {
             </div>
             <div>
                 <div className={styles.contactIcons}>
-                    <a href="https://www.linkedin.com/in/collins-makui-2b36341a2/" className={styles.contactIcon}><FaLinkedin /></a>
-                    <a href="https://github.com/Makcollins" className={styles.contactIcon}><FaGithub /></a>
+                    <a href="https://www.linkedin.com/in/collins-makui/" className={styles.contactIcon}><FaLinkedin /></a>
+                    <a href="https://github.com/devcollinsmakui" className={styles.contactIcon}><FaGithub /></a>
+                    <a href="https://github.com/Makcollins" className={styles.contactIcon}><FaGithubAlt /></a>
 {/*                     <a href="mailto:makcollins16@gmail.com" className={styles.contactIcon}><SiGmail /></a> */}
                 </div>
             </div>
